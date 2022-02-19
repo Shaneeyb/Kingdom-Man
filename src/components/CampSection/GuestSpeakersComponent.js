44d4251da57bb770dc330c621c3ded1faf5d631d
Shane Burke <shaneburkework@gmail.com>
2022-02-18 14:53:46 -1000

    replaced guest speakers carousel with cards

--- src/components/CampSection/GuestSpeakersComponent.js
+++ src/components/CampSection/GuestSpeakersComponent.js
@@ -1,62 +1,92 @@
-import React from 'react';
-import Carousel from 'react-bootstrap/Carousel'
+import React from "react";
+import Carousel from "react-bootstrap/Carousel";
+import {
+  Card,
+  Button,
+  CardImg,
+  CardTitle,
+  CardText,
+  CardGroup,
+  CardSubtitle,
+  CardBody,
+} from "reactstrap";
+
 //import kenobi from "./src/images/kenobi.jpg"
 
 function Title() {
-    return (
-        <h2>Guest Speakers</h2>
-    )
+  return <h2>Guest Speakers</h2>;
 }
 
 function Pictures() {
-    return (
-        <div>
-            <Carousel variant="light">
-  <Carousel.Item>
-    <img
-    src="https://assets3.thrillist.com/v1/image/2928355/1536x1024/crop;webp=auto;jpeg_quality=60;progressive.jpg" 
-    alt="Obi-Wan"
-      className="d-block w-100"
-    />
-    <Carousel.Caption>
-      <h5>Obi-Wan Kenobi</h5>
-      <p>The greatest of all times!!!</p>
-    </Carousel.Caption>
-  </Carousel.Item>
-  <Carousel.Item>
-    <img
-     src="https://static1.cbrimages.com/wordpress/wp-content/uploads/2020/12/One-Punch-Man-Saitama-Says-OK.jpg?q=50&fit=crop&w=740&h=370&dpr=1.5" 
-     alt="Saitama" 
-      className="d-block w-100"
-    />
-    <Carousel.Caption>
-      <h5>Saitama</h5>
-      <p>Aka, One Punch Man</p>
-    </Carousel.Caption>
-  </Carousel.Item>
-  <Carousel.Item>
-    <img
-    src="https://preview.redd.it/qwwri9fw4b471.png?width=853&format=png&auto=webp&s=43739004a428a020f95d7334e808d007f1394e68"
-    alt="Bokuto"
-      className="d-block w-100"
-    />
-    <Carousel.Caption>
-      <h5>Oikawa</h5>
-      <p>The Great King.</p>
-    </Carousel.Caption>
-  </Carousel.Item>
-</Carousel>
-        </div>
-    )
+  return (
+    <div>
+      <CardGroup>
+        <Card>
+          <CardImg
+            top
+            width="100%"
+            src="https://assets3.thrillist.com/v1/image/2928355/1536x1024/crop;webp=auto;jpeg_quality=60;progressive.jpg"
+            alt="Obi-Wan"
+          />
+          <CardBody>
+            <b>
+              <CardTitle>Obi-Wan Kenobi</CardTitle>{" "}
+            </b>
+            <CardSubtitle>the Negotiator. </CardSubtitle>
+            <CardText>
+              Coming all the way from Ewa beach is Pastor _____
+            </CardText>
+            <Button>Pastor _____</Button>
+          </CardBody>
+        </Card>
+        <Card>
+          <CardImg
+            top
+            width="100%"
+            src="https://static1.cbrimages.com/wordpress/wp-content/uploads/2020/12/One-Punch-Man-Saitama-Says-OK.jpg?q=50&fit=crop&w=740&h=370&dpr=1.5"
+            alt="Saitama"
+          />
+          <CardBody>
+            <b>
+              <CardTitle>Saitama</CardTitle>{" "}
+            </b>
+            <CardSubtitle>Caped Baldy</CardSubtitle>
+            <CardText>
+              Coming all the way from Ewa beach is Pastor _____
+            </CardText>
+            <Button>Pastor _____</Button>
+          </CardBody>
+        </Card>
+        <Card>
+          <CardImg
+            top
+            width="100%"
+            src="https://preview.redd.it/qwwri9fw4b471.png?width=853&format=png&auto=webp&s=43739004a428a020f95d7334e808d007f1394e68"
+            alt="Bokuto"
+          />
+          <CardBody>
+            <b>
+              <CardTitle>Oikawa</CardTitle>{" "}
+            </b>
+            <CardSubtitle>The Great King</CardSubtitle>
+            <CardText>
+              Coming all the way from Ewa beach is Pastor _____
+            </CardText>
+            <Button>Pastor _____</Button>
+          </CardBody>
+        </Card>
+      </CardGroup>
+    </div>
+  );
 }
 
 function GuestSpeakers() {
-    return (
-        <div>
-            <Title />
-            <Pictures />
-        </div>
-    )
+  return (
+    <div>
+      <Title />
+      <Pictures />
+    </div>
+  );
 }
 
 export default GuestSpeakers;
--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -1,77 +1,76 @@
 import React from "react";
 import Register from "./RegisterComponent";
 //import CampfireBG from "/images/CampfireBG.jpg"
-    import { useNavigate } from "react-router-dom"
+import { useNavigate } from "react-router-dom";
 
 function Home() {
-
   let navigate = useNavigate();
 
-
   function HeaderText() {
-
-const styles = {
-  main: {
-    textAlign: 'center',
-    fontWeight: 'bold',
-    padding: "60px"
-  },
-  date: {
-    textAlign: "center",
-    padding: "8px"
-  }
-}
+    const styles = {
+      main: {
+        textAlign: "center",
+        fontWeight: "bold",
+        padding: "60px",
+      },
+      date: {
+        textAlign: "center",
+        padding: "8px",
+      },
+    };
 
     return (
       <div>
         <div className="container">
-            <div className="row">
-              <div className="mx-auto">
-        <h1 style={styles.main}>Kingdom Man</h1>
-        <h4 style={styles.date}>Jan 1st - 3rd. 2022</h4>
-        <h4 style={styles.date}>94-877 Lumiaina St, Waipahu, HI 96797 </h4>
-              </div>
+          <div className="row">
+            <div className="mx-auto">
+              <h1 style={styles.main}>Kingdom Man</h1>
+              <h4 style={styles.date}>Jan 1st - 3rd. 2022</h4>
+              <h4 style={styles.date}>
+                94-877 Lumiaina St, Waipahu, HI 96797{" "}
+              </h4>
             </div>
           </div>
+        </div>
       </div>
     );
   }
 
   function RegisterBtn() {
-
     return (
-      <div style={{
-        display: 'flex',  
-        justifyContent:'center',
-        alignItems:'center', 
-        padding: "60px"
-        
-        }}>
-          <button 
-          className="btn btn-danger" 
-          onClick={() => navigate("/RegisterComponent"
-          )}
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          padding: "60px",
+        }}
+      >
+        <button
+          className="btn btn-danger"
+          onClick={() => navigate("/RegisterComponent")}
           style={{
             padding: "3vh",
-          border: "none",
-          height: "100px",
-          width: "300px",
-          fontSize: "60px",
-          justifyContent: "center",
-          textAlign: "center"
+            border: "none",
+            height: "80px",
+            width: "300px",
+            fontSize: "60px",
+            justifyContent: "center",
+            textAlign: "center",
           }}
-            >Register</button>
+        >
+          Register
+        </button>
       </div>
     );
   }
 
-    return(
-      <div
-        >
-        <HeaderText />
-        <RegisterBtn />
-      </div>
-    )
+  return (
+    <div>
+      <HeaderText />
+      <RegisterBtn />
+    </div>
+  );
 }
 
 export default Home;
