8e129b189947a8c6ae42de10329bfcd78db5c7cf
Shane Burke <shaneburkework@gmail.com>
2022-02-23 15:16:52 -1000

    trying to make the video responsive

--- src/components/CampSection/VideoComponent.js
+++ src/components/CampSection/VideoComponent.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 //import Flexbox from 'flexbox-react';
 
 // width="100%" height="500"
@@ -14,38 +14,98 @@ div style={{
             flexDirection: "column"
 
         }}>
+
+
+
+iframe {
+    min-height: 100%;
+    min-width: 100%;
+    display: flex;
+    flex-flow: column;
+    justify-content: center;
+    flex: auto;
+    align-items: center;
+}
+
+
+
+<div style="padding:56.25% 0 0 0;position:relative;">
+  <iframe
+    src="https://player.vimeo.com/video/328889318?h=7476b833b8&title=0&byline=0&portrait=0" 
+    frameBorder="0"
+    allow="autoplay"
+    allow="fullscreen"
+    allow= "picture-in-picture"
+    allowFullScreen
+    style={{
+        position: "absolute",
+        top: "0",
+        left: "0",
+        width: "100%",
+        height: "100%"
+      }}
+    title="Kingdom Man Camp 2019 Recap"
+  ></iframe>
+</div>
+
+
 */
 
 function KMVideo() {
-    return (
-        <div className="container h-100">
-            <div>
-                    <iframe title="vimeo-player" src="https://player.vimeo.com/video/328889318?h=7476b833b8" frameBorder="0" allowFullScreen></iframe>
-            </div>
-        </div>
-    )
+  return (
+    <div>
+      <div
+        style={{
+          padding: "56.25% 0 0 0",
+          position: "relative",
+        }}
+      >
+        <iframe
+          src="https://player.vimeo.com/video/328889318?h=7476b833b8&title=0&byline=0&portrait=0"
+          frameBorder="0"
+          allow="autoplay"
+          allow="fullscreen"
+          allow="picture-in-picture"
+          allowFullScreen
+          style={{
+            position: "absolute",
+            top: "0",
+            left: "0",
+            width: "100%",
+            height: "100%",
+          }}
+          title="Kingdom Man Camp 2019 Recap"
+        ></iframe>
+      </div>
+    </div>
+  );
 }
 
-
 function Text() {
-    return (
-        <div>
-            <p style={{
-        padding: "30px"
-      }}>
-            Kingdom Man Ministry at Inspire Church is leading the way in Hawaii to reach men and teach them how to live an authentic life as Jesus did. We partner with the Men’s 33 Series to equip each man with the necessary tools to help them discover what it means to live with a generational vision.
-            </p>
-        </div>
-    )
+  return (
+    <div>
+      <p
+        style={{
+          padding: "30px",
+        }}
+      >
+        Kingdom Man Ministry at Inspire Church is leading the way in Hawaii to
+        reach men and teach them how to live an authentic life as Jesus did. We
+        partner with the Men’s 33 Series to equip each man with the necessary
+        tools to help them discover what it means to live with a generational
+        vision.
+      </p>
+    </div>
+  );
 }
 
 function Video() {
-    return (
-        <div>
-            <KMVideo />
-            <Text />
-        </div>
-    )
+  return (
+    <div>
+      <KMVideo />
+      <Text />
+    </div>
+  );
 }
 
 export default Video;
